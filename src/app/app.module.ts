767e58ec1473b4257005dc0a6573ef0fed4bd27e
piraces <raul.piraces@gmail.com>
2018-08-07 14:58:26 +0200

    Style changes, and events from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
@@ -34,6 +35,7 @@ import { AdMobPro } from '@ionic-native/admob-pro';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp, {
       backButtonText: 'Volver',
     }),
